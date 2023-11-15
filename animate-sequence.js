468a5b1a7b5b0e9dc249b62ea7921191da6eda37
Luke <lukekumar3@gmail.com>
2023-11-14 22:58:03 -0500

    init; animate-sequence JS fn

---
+++ animate-sequence.js
@@ -0,0 +1,26 @@
+export function update-sequence-animation(arr, stateCallback, cumulative, loopStart) {
+
+	if (cumulative) {
+		for (let i = loopStart; i < arr.length; i++) {
+			if (arr[i] == false) {
+				arr[i] = true;
+				break;
+			} else if (i == arr.length - 1) {
+				arr = [...Array(arr.length)].map(() => false);
+			}
+		}
+
+	} else {
+		arr = arr.map((itm, idx) => {
+			if (itm == false && ((!arr[idx -1] && arr[arr.length - 1] == true) || arr[idx - 1] == true) {
+				return true;
+			} else if (idx == 0 && arr.find((val) => val == true)) == null {
+				return true;
+			} else {
+				return false;
+			}
+		});
+	}
+
+	stateCallback(arr);
+}

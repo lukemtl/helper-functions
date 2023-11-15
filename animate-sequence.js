266e37bc7d8b2db37d0ed66302c7e984f7a59f52
Luke <lukekumar3@gmail.com>
2023-11-15 09:51:55 -0500

    re-implement map/merge functions with copy array

--- animate-sequence.js
+++ animate-sequence.js
@@ -1,24 +1,19 @@
-export function updateSequenceAnimation(arr, cumulative, loopStart, stateCallback) {
+export function updateSequenceAnimation(arr, cumulative, loopStart, callback) {
+	// *** map/merge functions wont update reference; must update original array in callback in adidtion to state update		
 	
+	let nextArr = [...arr];
 
 	if (cumulative) {
 		for (let i = loopStart; i < arr.length; i++) {
-			console.log(i, arr);
-			if (arr[i] == false) {
-				arr[i] = true;
-				console.log("break")
+			if (nextArr[i] == false) {
+				nextArr[i] = true;
 				break;
 			} else if (i == arr.length - 1) {
-				// *** map/merge function wont update reference
-				for (let j = 0; j < arr.length; j++) {
-					arr[j] = false;
-				}
-				console.log("redo:", arr)
-				
+				nextArr = arr.map(() => false);
 			}
 		}
 	} else {
-		arr = arr.map((itm, idx) => {
+		nextArr = arr.map((itm, idx) => {
 			if (itm == false && ((!arr[idx -1] && arr[arr.length - 1] == true) || arr[idx - 1] == true)) {
 				return true;
 			} else if (idx == 0 && arr.find((val) => val == true) == null) {
@@ -29,5 +24,5 @@ export function updateSequenceAnimation(arr, cumulative, loopStart, stateCallbac
 		});
 	}
 
-	stateCallback(arr);
+	callback(nextArr);
 }
